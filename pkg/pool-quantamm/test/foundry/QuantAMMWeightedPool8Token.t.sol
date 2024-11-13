96213a04d7d6d8190dbc1c32c0f0a402f06fedc1
christian harrington <christian@bulkbit.systems>
2024-11-13 23:19:19 +0000

    e2e balancer tests

--- pkg/pool-quantamm/test/foundry/QuantAMMWeightedPool8Token.t.sol
+++ pkg/pool-quantamm/test/foundry/QuantAMMWeightedPool8Token.t.sol
@@ -38,11 +38,6 @@ contract QuantAMMWeightedPool8TokenTest is QuantAMMWeightedPoolContractsDeployer
     uint64 public constant MAX_SWAP_FEE_PERCENTAGE = 10e16;
 
     QuantAMMWeightedPoolFactory internal quantAMMWeightedPoolFactory;
-    MockUpdateWeightRunner internal updateWeightRunner;
-    MockChainlinkOracle internal chainlinkOracle;
-    address internal owner;
-    address internal addr1;
-    address internal addr2;
 
     function setUp() public override {
         int216 fixedValue = 1000;
@@ -67,8 +62,7 @@ contract QuantAMMWeightedPool8TokenTest is QuantAMMWeightedPoolContractsDeployer
             IVault(address(vault)),
             365 days,
             "Factory v1",
-            "Pool v1",
-            address(updateWeightRunner)
+            "Pool v1"
         );
         vm.label(address(quantAMMWeightedPoolFactory), "quantamm weighted pool factory");
     }
@@ -470,11 +464,6 @@ contract QuantAMMWeightedPool8TokenTest is QuantAMMWeightedPoolContractsDeployer
         testParam memory firstWeight = testParam(5, 0.1e18, 0.001e18);
         testParam memory secondWeight = testParam(7, 0.15e18, 0.001e18);
         _onSwapInGivenOutInternal(firstWeight, secondWeight, 7, 2.214834140775105000e18);
-    }
-
-    function _deployOracle(int216 fixedValue, uint delay) internal returns (MockChainlinkOracle) {
-        MockChainlinkOracle oracle = new MockChainlinkOracle(fixedValue, delay);
-        return oracle;
     }
 
     function _getDefaultBalances() internal pure returns (uint256[] memory balances) {

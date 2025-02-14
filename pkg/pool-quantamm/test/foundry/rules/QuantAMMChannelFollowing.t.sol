e4d9f4f45eb715d708296011c5742045815a29db
christian harrington <christian@bulkbit.systems>
2025-02-14 17:18:28 +0000

    CODEHAWKS H-08 add test for channel following

--- pkg/pool-quantamm/test/foundry/rules/QuantAMMChannelFollowing.t.sol
+++ pkg/pool-quantamm/test/foundry/rules/QuantAMMChannelFollowing.t.sol
@@ -755,4 +755,127 @@ contract ChannelFollowingUpdateRuleTest is Test, QuantAMMTestUtils {
 
         checkResult(resultWeights, expectedResults);
     }
+
+    struct FuzzRuleParams {
+        int256 numAssets;
+        bool vectorParams;
+        bool vectorQ;
+        int256 lambda;
+        int256 kappa;
+        int256 width;
+        int256 amplitude;
+        int256 exponents;
+        int256 inverseScaling;
+        int256 preExponentialScaling;
+        int256 prevWeight;
+        int256 prevMovingAverage;
+        int256 prevAlpha;
+        int256 data;
+        bool useRawPriceDefined;
+        bool useRawPrice;
+    }
+
+    function testFuzz_channelFollowing_reasonableRanges(FuzzRuleParams memory params) public {
+        // Define local variables for the parameters
+        uint boundNumAssets = uint(bound(params.numAssets, 2, 8));
+        uint boundNumParameters = params.vectorParams ? boundNumAssets : 1;
+
+        int256[][] memory parameters = new int256[][](7);
+
+        //Kappa
+        parameters[0] = new int256[](boundNumParameters);
+        for(uint i = 0; i < boundNumParameters; i++){
+            parameters[0][i] = PRBMathSD59x18.fromInt(bound(params.kappa, 1, 5));
+        }
+
+        //Width
+        parameters[1] = new int256[](boundNumParameters);
+        for(uint i = 0; i < boundNumParameters; i++){
+            parameters[1][i] = PRBMathSD59x18.fromInt(bound(params.width, 1, 100));
+            vm.assume(parameters[1][i] != 0);
+        }
+
+        //Amplitude
+        parameters[2] = new int256[](boundNumParameters);
+        for(uint i = 0; i < boundNumParameters; i++){
+            parameters[2][i] = PRBMathSD59x18.fromInt(bound(params.amplitude, 1, 10));
+        }
+
+        //exponents
+        parameters[3] = new int256[](boundNumParameters);
+        for(uint i = 0; i < boundNumParameters; i++){
+            parameters[3][i] = PRBMathSD59x18.fromInt(bound(params.exponents, 1, 10));
+            vm.assume(parameters[3][i] != 0);
+        }
+
+        // Inverse Scaling
+        parameters[4] = new int256[](boundNumParameters);
+        for(uint i = 0; i < boundNumParameters; i++){
+            parameters[4][i] = 0.5415e18;
+        }
+
+        //Pre-exp Scaling
+        parameters[5] = new int256[](boundNumParameters);
+        for(uint i = 0; i < boundNumParameters; i++){
+            parameters[5][i] = PRBMathSD59x18.fromInt(bound(params.preExponentialScaling, 0, 100));
+            vm.assume(parameters[5][i] != 0);
+        }
+
+        parameters[6] = new int256[](1);
+        int256 useRawPriceInt = params.useRawPrice ? PRBMathSD59x18.fromInt(1) : PRBMathSD59x18.fromInt(0);
+        parameters[6][0] = PRBMathSD59x18.fromInt(useRawPriceInt);
+        
+
+        int256[] memory previousAlphas = new int256[](boundNumAssets);
+        for(uint i = 0; i < boundNumAssets; i++){
+            previousAlphas[i] = PRBMathSD59x18.fromInt(bound(params.prevAlpha, -10000000000, 10000000000));
+        }
+
+        int256[] memory prevMovingAverages = new int256[](boundNumAssets);
+        for(uint i = 0; i < boundNumAssets; i++){
+            prevMovingAverages[i] = PRBMathSD59x18.fromInt(bound(params.prevMovingAverage, 1, 10000000000));
+        }
+
+        int256[] memory movingAverages = new int256[](boundNumAssets);
+        for(uint i = 0; i < boundNumAssets; i++){
+            movingAverages[i] = PRBMathSD59x18.fromInt(bound(params.prevMovingAverage, 1, 10000000000));
+        }
+
+        int128[] memory lambdas = new int128[](boundNumParameters);
+        for(uint i = 0; i < boundNumParameters; i++){
+            lambdas[i] = int128(int256(bound(params.lambda, 0.5e18, 0.99999e18)));
+        }
+
+        int256[] memory prevWeights = new int256[](boundNumAssets);
+        for (uint i = 0; i < boundNumAssets; i++) {
+            prevWeights[i] = PRBMathSD59x18.fromInt(bound(params.prevWeight, 0.01e18, 0.99e18));
+        }
+
+        int256 totalWeight = 0;
+        for (uint i = 0; i < boundNumAssets; i++) {
+            totalWeight += prevWeights[i];
+        }
+
+        for (uint i = 0; i < boundNumAssets; i++) {
+            prevWeights[i] = (prevWeights[i] * 1e18) / totalWeight;
+        }
+
+        int256[] memory oracleData = new int256[](boundNumAssets);
+        for(uint i = 0; i < boundNumAssets; i++){
+            oracleData[i] = PRBMathSD59x18.fromInt(bound(params.data, 1, 10000000000));
+        }       
+
+        mockPool.setNumberOfAssets(boundNumAssets);
+
+        // Call the rule function and get the result
+        rule.initialisePoolRuleIntermediateValues(
+            address(mockPool),
+            prevMovingAverages,
+            previousAlphas,
+            mockPool.numAssets()
+        );
+
+        rule.CalculateUnguardedWeights(prevWeights, oracleData, address(mockPool), parameters, lambdas, movingAverages);
+
+    }
 }

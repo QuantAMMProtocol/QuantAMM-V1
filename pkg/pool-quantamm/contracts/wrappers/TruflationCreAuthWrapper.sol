2ac44d649c4143915f5e453db87ff074bf3a7d6f
christian harrington <christian@bulkbit.systems>
2025-11-13 11:03:47 +0000

    remove weight checks given potential smoothing

--- pkg/pool-quantamm/contracts/wrappers/TruflationCreAuthWrapper.sol
+++ pkg/pool-quantamm/contracts/wrappers/TruflationCreAuthWrapper.sol
@@ -5,57 +5,33 @@ import { ITypeAndVersion } from "@chainlink/contracts/src/v0.8/shared/interfaces
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IUpdateWeightRunner } from "@balancer-labs/v3-interfaces/contracts/pool-quantamm/IUpdateWeightRunner.sol";
 
-contract TruflationCreAuthWrapper is ITypeAndVersion, Ownable {
-    constructor(
-        address updateWeightRunnerAddress,
-        int256[] memory fullInflationRegimeWeight,
-        int256[] memory mixedInflationRegimeWeight,
-        int256[] memory noInflationRegimeWeight
-    ) Ownable(msg.sender) {
+/// @title The Wrapper needed to bridge between CRE auth and update weighr runner
+/// @dev CRE has a single address per chain that can execute functions.
+///      While this means that msg.sender can be constant it also means anyone can setup
+///      a CRE workflow and it will trigger from the same msg.sender.
+///      The metadata is set by the workflow sender and cannot be manipulated.
+///      Checking the metadata as well as the msg.sender means that the auth is complete
+contract CREAuthWrapper is ITypeAndVersion, Ownable {
+    constructor(address updateWeightRunnerAddress) Ownable(msg.sender) {
         updateWeightRunner = IUpdateWeightRunner(updateWeightRunnerAddress);
-
-        require(fullInflationRegimeWeight.length == 2, "Invalid Full inflation regime weight");
-        require(mixedInflationRegimeWeight.length == 2, "Invalid Mixed inflation regime weight");
-        require(noInflationRegimeWeight.length == 2, "Invalid No inflation regime weight");
-
-        BTC_FULL_INFLATION_REGIME_WEIGHT = fullInflationRegimeWeight[0];
-        USDC_FULL_INFLATION_REGIME_WEIGHT = fullInflationRegimeWeight[1];
-        BTC_MIXED_INFLATION_REGIME_WEIGHT = mixedInflationRegimeWeight[0];
-        USDC_MIXED_INFLATION_REGIME_WEIGHT = mixedInflationRegimeWeight[1];
-        BTC_NO_INFLATION_REGIME_WEIGHT = noInflationRegimeWeight[0];
-        USDC_NO_INFLATION_REGIME_WEIGHT = noInflationRegimeWeight[1];
     }
 
     IUpdateWeightRunner public updateWeightRunner;
 
-    uint40 public lastInterpolationTimePossible;
-
     string public constant override typeAndVersion = "CreAuthWrapper 1.0.0";
 
-    int256 public immutable BTC_FULL_INFLATION_REGIME_WEIGHT;
-    int256 public immutable USDC_FULL_INFLATION_REGIME_WEIGHT;
-    int256 public immutable BTC_MIXED_INFLATION_REGIME_WEIGHT;
-    int256 public immutable USDC_MIXED_INFLATION_REGIME_WEIGHT;
-    int256 public immutable BTC_NO_INFLATION_REGIME_WEIGHT;
-    int256 public immutable USDC_NO_INFLATION_REGIME_WEIGHT;
-
     struct WorkflowMetadata {
+        address poolAddress;
         address allowedSender;
         address allowedWorkflowOwner;
         bytes10 allowedWorkflowName;
     }
 
-    bytes32 public s_writePermission;
+    WorkflowMetadata public s_writePermission;
 
-    event WritePermissionRemoved(bytes32 indexed pool);
-    event TargetWeightsForwarded(
-        uint indexed regime,
-        address indexed pool,
-        address sender,
-        address workflowOwner,
-        bytes10 workflowName
-    );
-    event WriteAdminSet(address indexed feedAdmin, bool indexed isAdmin);
+    event WritePermissionRemoved(WorkflowMetadata indexed pool);
+    event TargetWeightsForwarded(address indexed pool, address sender, address workflowOwner, bytes10 workflowName);
+    event WriteAdminSet(address indexed poolAdmin, bool indexed isAdmin);
     event InvalidUpdatePermission(address indexed pool, address sender, address workflowOwner, bytes10 workflowName);
 
     error EmptyConfig();
@@ -64,20 +40,26 @@ contract TruflationCreAuthWrapper is ITypeAndVersion, Ownable {
     error UnauthorizedCaller(address caller);
 
     /// @notice Checks to see if this data ID, msg.sender, workflow owner, and workflow name are permissioned
-    /// @param pool The address of the pool for the feed
     /// @param workflowMetadata workflow metadata
-    function checkFeedPermission(
-        address pool,
+    function checkCREAuthentication(
         WorkflowMetadata memory workflowMetadata
     ) external view returns (bool hasPermission) {
-        bytes32 permission = _createPoolHash(
-            pool,
-            workflowMetadata.allowedSender,
-            workflowMetadata.allowedWorkflowOwner,
-            workflowMetadata.allowedWorkflowName
-        );
+        return checkCREAuth(workflowMetadata, s_writePermission);
+    }
 
-        return s_writePermission == permission;
+    /// @notice Internal function to check CRE authentication
+    /// @param workflowMetadata workflow metadata
+    /// @param permissionWorkflowMetaData permissioned workflow metadata by the owner
+    /// @return hasPermission boolean indicating if permission is granted
+    function checkCREAuth(
+        WorkflowMetadata memory workflowMetadata,
+        WorkflowMetadata memory permissionWorkflowMetaData
+    ) internal pure returns (bool hasPermission) {
+        return
+            workflowMetadata.allowedSender == permissionWorkflowMetaData.allowedSender &&
+            workflowMetadata.allowedWorkflowOwner == permissionWorkflowMetaData.allowedWorkflowOwner &&
+            workflowMetadata.allowedWorkflowName == permissionWorkflowMetaData.allowedWorkflowName &&
+            workflowMetadata.poolAddress == permissionWorkflowMetaData.poolAddress;
     }
 
     /// @notice Sets the UpdateWeightRunner contract address. Admin breakglass feature available to owner only.
@@ -87,10 +69,9 @@ contract TruflationCreAuthWrapper is ITypeAndVersion, Ownable {
         updateWeightRunner = IUpdateWeightRunner(updateWeightRunnerAddress);
     }
 
-    /// @notice Initializes the config for a pool feed
-    /// @param pool The address of the pool for the feed
-    /// @param workflowMetadata List of workflow metadata (owners, senders, and names) for every feed
-    function setWriteConfig(address pool, WorkflowMetadata calldata workflowMetadata) external onlyOwner {
+    /// @notice Initializes the config for a pool
+    /// @param workflowMetadata List of workflow metadata (poolAddress, owners, senders, and names)
+    function setWriteConfig(WorkflowMetadata calldata workflowMetadata) external onlyOwner {
         if (workflowMetadata.allowedSender == address(0)) {
             revert InvalidAddress(workflowMetadata.allowedSender);
         }
@@ -101,30 +82,32 @@ contract TruflationCreAuthWrapper is ITypeAndVersion, Ownable {
             revert InvalidWorkflowName(workflowMetadata.allowedWorkflowName);
         }
 
-        bytes32 poolHash = _createPoolHash(
-            pool,
-            workflowMetadata.allowedSender,
-            workflowMetadata.allowedWorkflowOwner,
-            workflowMetadata.allowedWorkflowName
-        );
+        if (workflowMetadata.poolAddress == address(0)) {
+            revert InvalidAddress(workflowMetadata.poolAddress);
+        }
 
-        s_writePermission = poolHash;
+        s_writePermission = workflowMetadata;
 
         emit WriteAdminSet(workflowMetadata.allowedSender, true);
     }
 
-    /// @notice Removes the write permission config for a pool feed
+    /// @notice Removes the write permission config for a pool
     function removeWritePermissionConfig() external onlyOwner {
-        if (s_writePermission == bytes32(0)) {
+        if (s_writePermission.allowedSender == address(0)) {
             revert EmptyConfig();
         }
 
         emit WritePermissionRemoved(s_writePermission);
 
-        s_writePermission = bytes32(0);
+        s_writePermission = WorkflowMetadata({
+            poolAddress: address(0),
+            allowedSender: address(0),
+            allowedWorkflowOwner: address(0),
+            allowedWorkflowName: bytes10(0)
+        });
     }
 
-    /// @notice Extracts the workflow name and the workflow owner from the metadata parameter of onReport
+    /// @notice Extracts the workflow name and the workflow owner from the metadata parameter of setTargetWeightsByRegimeWithMeta
     /// @param metadata The metadata in bytes format
     /// @return workflowOwner The owner of the workflow
     /// @return workflowName  The name of the workflow
@@ -146,65 +129,38 @@ contract TruflationCreAuthWrapper is ITypeAndVersion, Ownable {
         return (workflowOwner, workflowName);
     }
 
-    /// @notice Creates a pool hash used to permission write access
-    /// @param poolId The pool ID for the feed
-    /// @param sender The msg.sender of the transaction calling into onpool
-    /// @param workflowOwner The owner of the workflow
-    /// @param workflowName The name of the workflow
-    /// @return poolHash The keccak256 hash of the abi.encoded inputs
-    function _createPoolHash(
-        address poolId,
-        address sender,
-        address workflowOwner,
-        bytes10 workflowName
-    ) internal pure returns (bytes32) {
-        return keccak256(abi.encode(poolId, sender, workflowOwner, workflowName));
-    }
-
-    enum InflationRegime {
-        BTC_INFLATION,
-        MIXED_INFLATION,
-        NO_INFLATION
-    }
-
+    /// @notice The main function that checks the CRE permissions and then passes through the weights
+    /// @param _poolAddress target pool address
+    /// @param _weights the new target weights
+    /// @param _lastInterpolationTimePossible the update interval
+    /// @param _meta the CRE metadata required to authenticate
+    /// @dev this wrapper assumes complete trust in the correct workflow in terms of weights and last interpolation time
     function setTargetWeightsByRegimeWithMeta(
-        uint newRegime,
         address _poolAddress,
         int256[] calldata _weights,
+        uint40 _lastInterpolationTimePossible,
         bytes calldata _meta
     ) external {
-        require(
-            newRegime == uint(InflationRegime.BTC_INFLATION) ||
-                newRegime == uint(InflationRegime.MIXED_INFLATION) ||
-                newRegime == uint(InflationRegime.NO_INFLATION),
-            "INVALID_REGIME"
-        );
-
         (address workflowOwner, bytes10 workflowName) = _getWorkflowMetaData(_meta);
 
-        bytes32 permission = _createPoolHash(_poolAddress, msg.sender, workflowOwner, workflowName);
+        WorkflowMetadata memory workflowMetadata = WorkflowMetadata({
+            poolAddress: _poolAddress,
+            allowedSender: msg.sender, //main check against the singleton CRE address
+            allowedWorkflowOwner: workflowOwner, //check against rogue workflows
+            allowedWorkflowName: workflowName
+        });
 
-        if (s_writePermission != permission) {
+        if (checkCREAuth(workflowMetadata, s_writePermission)) {
+            updateWeightRunner.setTargetWeightsManually(
+                _weights,
+                _poolAddress,
+                _lastInterpolationTimePossible,
+                _weights.length
+            );
+            emit TargetWeightsForwarded(_poolAddress, msg.sender, workflowOwner, workflowName);
+        } else {
             emit InvalidUpdatePermission(_poolAddress, msg.sender, workflowOwner, workflowName);
             revert UnauthorizedCaller(msg.sender);
         }
-
-        if (newRegime == uint40(InflationRegime.BTC_INFLATION)) {
-            require(_weights.length == 2, "ARRAY_LENGTH_MISMATCH");
-            require(_weights[0] == BTC_FULL_INFLATION_REGIME_WEIGHT, "INVALID_WEIGHT");
-            require(_weights[1] == USDC_FULL_INFLATION_REGIME_WEIGHT, "INVALID_WEIGHT");
-        } else if (newRegime == uint40(InflationRegime.MIXED_INFLATION)) {
-            require(_weights.length == 2, "ARRAY_LENGTH_MISMATCH");
-            require(_weights[0] == BTC_MIXED_INFLATION_REGIME_WEIGHT, "INVALID_WEIGHT");
-            require(_weights[1] == USDC_MIXED_INFLATION_REGIME_WEIGHT, "INVALID_WEIGHT");
-        } else {
-            require(_weights.length == 2, "ARRAY_LENGTH_MISMATCH");
-            require(_weights[0] == BTC_NO_INFLATION_REGIME_WEIGHT, "INVALID_WEIGHT");
-            require(_weights[1] == USDC_NO_INFLATION_REGIME_WEIGHT, "INVALID_WEIGHT");
-        }
-
-        updateWeightRunner.setTargetWeightsManually(_weights, _poolAddress, lastInterpolationTimePossible, uint(2));
-
-        emit TargetWeightsForwarded(newRegime, _poolAddress, msg.sender, workflowOwner, workflowName);
     }
 }
